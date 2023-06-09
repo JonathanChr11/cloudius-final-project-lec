8a77c37ed4918dd38598ab095e2d5adc93b99431
JonathanChr11 <jonathansoesanto@gmail.com>
2023-06-09 23:46:10 +0700

    feat: added validation to billing domain, billing hosting and contact page

---
+++ js/hosting/billing-hosting.js
@@ -0,0 +1,21 @@
+function purchaseValidation() {
+    var hostingPlan = document.getElementById("hosting-plan").value;
+    var hostingEmail = document.getElementById("hosting-email").value;
+    var hostingValidationPlan = document.getElementById(
+        "hosting-validation-plan"
+    );
+    var hostingValidationEmail = document.getElementById(
+        "hosting-validation-email"
+    );
+
+    if (hostingPlan === "choose") {
+        hostingValidationPlan.classList.remove("visually-hidden");
+    } else {
+        hostingValidationPlan.classList.add("visually-hidden");
+    }
+    if (hostingEmail == "") {
+        hostingValidationEmail.classList.remove("visually-hidden");
+    } else {
+        window.location.href = "index.html";
+    }
+}
